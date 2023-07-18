a7da5181c4879d50ad193a6ac29094915f8795dd
Anthony Zamora <zamora.anthony1234@gmail.com>
2023-07-17 20:22:13 -0400

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("Anthony")));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.purple, Colors.redAccent, Colors.black],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Anthony",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28.0,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
